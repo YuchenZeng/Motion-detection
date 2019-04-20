92bc02c8ffa535d3fddc73ebfa90a6386fcf9c41
YuchenZeng <751497738@qq.com>
2019-04-20 18:22:18 -0400

    Insert text for 4 different video

--- main.m
+++ main.m
@@ -12,15 +12,24 @@ output2 = simple_frame_diff_function(v,row,col,length);
 output3 = adaptive_background_sub_function(v,row,col,length);
 output4 = persistent_frame_diff_function(v,row,col,length);
 
+%text insert information
+context = {'simple_BG_diff','adaptive_BG_sub','simple_frame_diff','persistent_frame_diff'};
+position = [10 10; 10 326; 250 10; 250 326];
+
 %combine 4 array into one array
 combine = zeros(2*row,2*col,length);
 for i = 1:length
+    
     image1 = output1(:,:,i);
     image2 = output2(:,:,i);
     image3 = output3(:,:,i);
     image4 = output4(:,:,i);
     %combine the image
-    combine(:,:,i) = [image1 image2;image3 image4];
+    image = [image1 image2;image3 image4];
+    %insert text
+    image = insertText(image, position, context);
+    %only take rgb = 1
+    combine(:,:,i) = image(:,:,1);
 end
 
 %output video
